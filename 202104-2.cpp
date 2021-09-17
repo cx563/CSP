7db70c230b73b44086a4b9b767ea50d8c2f4cc95
cx563 <3137400769@qq.com>
2021-09-17 11:09:27 +0800

    Update

--- 202104-2.cpp
+++ 202104-2.cpp
@@ -1,43 +1,35 @@
-#include <bits/stdc++.h>
-using namespace std;
-using gg = long long;
-int main()
-{
-    ios::sync_with_stdio(false);
+#include<bits/stdc++.h>    //   可以借鉴一维前缀和的思想  前缀和矩阵均是固定起点 考虑递推
+using namespace std;       //   但是在求一端具体的区间元素之和时，区间的起点是很重要的
+using gg = long long;      //   求二维前缀和矩阵时是根据矩阵右下角(x2,y2) - 1进行划分的 而求子矩阵的和时是根据左上角
+int main(void){            //   坐标(x1,y1)-1 进行划分的 始终记住定义 v[x][y] 表示以(1,1)为左上角 (x,y)为右下角
+    ios::sync_with_stdio(false);   //  的矩阵和 那划分过后，要求多余矩阵的和，显然也要根据这个定义 两个角的坐标都要参与
     cin.tie(0);
-    gg ni, li, ri, ti;
-    cin >> ni >> li >> ri >> ti;
-    vector<vector<gg>> v(ni, vector<gg>(ni + 1));
-    for (auto &i : v)
-    {
-        for (gg j = 1; j <= ni; j++)
-        {
-            cin >> i[j];
-        }
-   }
-    for(auto& i : v){
-        for (gg j = 1; j <= ni;j++){    //  前缀和
-            i[j] = i[j - 1] + i[j];
+    gg ni, li, ri, ti, res = 0;
+    cin>>ni>>li>>ri>>ti;
+    vector<vector<gg>>v(ni+1,vector<gg>(ni+1,0));
+    for(gg i =1;i<=ni;i++){
+        for(gg j = 1;j <= ni;j++){
+            cin>>v[i][j];
         }
     }
-    gg tmp = 0;
-    gg res = 0;
-    for (gg i = 0; i < ni;i++){
-        for (gg j = 1; j <= ni;j++){
-            tmp = 0;
-            gg leftup = (j - ri < 1 ? 1 : j - ri);
-            gg rightup = (j + ri > ni ? ni : j + ri);
-            gg leftdown = (i - ri < 0 ? 0 : i - ri);
-            gg rightdown = (i + ri > ni - 1 ? ni - 1 : i + ri);
-            gg num = (rightdown - leftdown + 1) * (rightup - leftup + 1);
-            while(leftdown <= rightdown){
-                tmp += (v[leftdown][rightup] - v[leftdown][leftup - 1]);
-                leftdown++;
-            }
-            if(tmp * 1.0 / num  <= ti)    //  要进行浮点比较 
-                res++;
+    //   构建二维前缀和数组
+    for(gg i =1;i<=ni;i++){
+        for(gg j =1;j<=ni;j++){
+            v[i][j] = v[i-1][j] + v[i][j-1] - v[i-1][j-1] + v[i][j];
         }
     }
-    cout << res;
+    //   求子矩阵元素和
+    for(gg i =1;i<=ni;i++){
+        for(gg j =1;j<=ni;j++){
+            gg x1 = i - ri < 1 ? 1 : i - ri;
+            gg y1 = j - ri < 1 ? 1 : j - ri;
+            gg x2 = i + ri > ni ? ni : i + ri;
+            gg y2 = j + ri > ni ? ni : j + ri;
+            gg ans = v[x2][y2] - v[x1-1][y2] - v[x2][y1-1] + v[x1-1][y1-1];
+            gg num = (x2-x1+1) * (y2 - y1 + 1);
+            if(ans <= ti * num) res++;
+        }
+    }
+    cout<<res;
     return 0;
 }
